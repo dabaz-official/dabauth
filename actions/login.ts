a971007c966fa11257757594cff35f8e99b7c193
DabAZ <dabaz@dabaz.me>
2024-05-24 17:52:52 +0800

    feat: add login functionality

--- actions/login.ts
+++ actions/login.ts
@@ -1,8 +1,11 @@
 "use server";
 
 import * as z from "zod";
+import { AuthError } from "next-auth";
 
 import { LoginSchema } from "@dabaz/schemas";
+import { signIn } from "@dabaz/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@dabaz/routes";
 
 export default async function login(values: z.infer<typeof LoginSchema>) {
   const validatedFields = LoginSchema.safeParse(values);
@@ -11,5 +14,23 @@ export default async function login(values: z.infer<typeof LoginSchema>) {
     return { error: "Invalid fields" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password } = validatedFields.data;
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    })
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials" };
+        default:
+          return { error: "Something went wrong!" };
+      }
+    };
+
+    throw error;
+  };
 };
--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,11 +1,24 @@
-import { auth } from "@dabaz/auth";
+import { auth, signOut } from "@dabaz/auth";
+
+export const metadata = {
+  title: "User Settings",
+}
 
 export default async function SettingsPage() {
   const session = await auth();
 
   return (
     <div>
-      Settings Page
+      {JSON.stringify(session)}
+      <form action={async () => {
+        "use server";
+
+        await signOut();
+      }}>
+        <button type="submit">
+          Sign out
+        </button>
+      </form>
     </div>
   );
 };
