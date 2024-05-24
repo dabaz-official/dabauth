d45d59d649e853800d63c20cf89235a2ac88d551
DabAZ <dabaz@dabaz.me>
2024-05-24 17:03:31 +0800

    feat: add register action

--- actions/register.ts
+++ actions/register.ts
@@ -1,15 +1,37 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcryptjs";
 
 import { RegisterSchema } from "@dabaz/schemas";
+import { db } from "@dabaz/lib/db";
+import { getUserByEmail } from "@dabaz/data/user";
 
 export default async function login(values: z.infer<typeof RegisterSchema>) {
   const validatedFields = RegisterSchema.safeParse(values);
 
   if (!validatedFields.success) {
-    return { error: "Invalid fields" };
+    return { error: "Invalid fields!" };
   }
+
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
 
   return { success: "Email sent!" };
 };
